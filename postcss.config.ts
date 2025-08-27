3b0acda54c062316bfe7c7420f247d86f9c49cdf
thinkdj <think.dj@gmail.com>
2025-08-27 23:56:43 +0530

    postcss.config.js

--- postcss.config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-} satisfies import('postcss').ProcessOptions & { plugins: Record<string, any> }
-
-export default config
